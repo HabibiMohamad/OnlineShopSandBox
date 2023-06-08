6da569a3354beaa22683017740975464da9849fa
mohammad habibi <lplnpfdfd@gmail.com>
2023-06-08 13:15:36 +0430

    added project

---
+++ MarketPlays/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MarketPlays.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Sass/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sass.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
